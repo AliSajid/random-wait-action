373588843c592844aeec760b2f945c73979c56a1
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-11-02 15:20:19 -0400

    feat: add the main body function

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,28 +2,21 @@ import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import {expect, test} from '@jest/globals'
+// import {expect, test} from '@jest/globals'
 
 test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
+  const minimum = parseInt('foo', 10)
+  const maximum = parseInt('bar', 10)
+  await expect(wait(minimum, maximum)).rejects.toThrow(
+    'minimum and maximum must be numbers'
+  )
 })
 
-test('wait 500 ms', async () => {
+test('wait between 1 and 5 seconds', async () => {
   const start = new Date()
-  await wait(500)
+  await wait(1, 5)
   const end = new Date()
   var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
+  expect(delta).toBeGreaterThan(1000)
+  expect(delta).toBeLessThan(10000)
 })
