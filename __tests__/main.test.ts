cd272169480e64fb1c22f6c70f8c24dbd326e7b6
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2022-11-02 17:22:53 -0400

    test: fixed the expectations in test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -18,5 +18,5 @@ test('wait between 1 and 5 seconds', async () => {
   const end = new Date()
   var delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThanOrEqual(1000)
-  expect(delta).toBeLessThanOrEqual(6000)
+  expect(delta).toBeLessThanOrEqual(5000)
 })
