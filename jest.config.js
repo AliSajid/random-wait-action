9045a11161d5e87c9c810c7d21b5d09156b345ca
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-03-20 15:41:14 -0400

    build: update things for pre-commit compliance
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
-  testTimeout: 30000,
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
-  verbose: true
+    clearMocks: true,
+    moduleFileExtensions: ['js', 'ts'],
+    testTimeout: 30000,
+    testMatch: ['**/*.test.ts'],
+    transform: {
+        '^.+\\.ts$': 'ts-jest'
+    },
+    verbose: true
 }
