859be089a1858586fc5a784800b847d8a8fb8d5f
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-03-20 15:41:14 -0400

    build: update things for pre-commit compliance
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { wait } from './wait'
 
 async function run(): Promise<void> {
-  try {
-    const minimum: string = core.getInput('minimum')
-    const maximum: string = core.getInput('maximum')
-    core.debug(`Waiting between ${minimum} and ${maximum} seconds...`)
+    try {
+        const minimum: string = core.getInput('minimum')
+        const maximum: string = core.getInput('maximum')
+        core.debug(`Waiting between ${minimum} and ${maximum} seconds...`)
 
-    core.debug(new Date().toTimeString())
-    const wait_time = await wait(parseInt(minimum), parseInt(maximum))
-    core.debug(new Date().toTimeString())
+        core.debug(new Date().toTimeString())
+        const wait_time = await wait(parseInt(minimum), parseInt(maximum))
+        core.debug(new Date().toTimeString())
 
-    core.setOutput('wait_time', wait_time)
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+        core.setOutput('wait_time', wait_time)
+    } catch (error) {
+        if (error instanceof Error) core.setFailed(error.message)
+    }
 }
 
 run()
