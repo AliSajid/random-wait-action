834f9261a26b43277468ccaf48521dbf111276b2
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-03-20 15:41:14 -0400

    build: update things for pre-commit compliance
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,22 +1,22 @@
-import {wait} from '../src/wait'
+import { wait } from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 // import {expect, test} from '@jest/globals'
 
 test('throws invalid number', async () => {
-  const minimum = parseInt('foo', 10)
-  const maximum = parseInt('bar', 10)
-  await expect(wait(minimum, maximum)).rejects.toThrow(
-    'minimum and maximum must be numbers'
-  )
+    const minimum = parseInt('foo', 10)
+    const maximum = parseInt('bar', 10)
+    await expect(wait(minimum, maximum)).rejects.toThrow(
+        'minimum and maximum must be numbers'
+    )
 })
 
 test('wait between 1 and 5 seconds', async () => {
-  const start = new Date()
-  await wait(1, 5)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThanOrEqual(1000)
-  expect(delta).toBeLessThanOrEqual(6000)
+    const start = new Date()
+    await wait(1, 5)
+    const end = new Date()
+    var delta = Math.abs(end.getTime() - start.getTime())
+    expect(delta).toBeGreaterThanOrEqual(1000)
+    expect(delta).toBeLessThanOrEqual(6000)
 })
--- src/wait.ts
+++ src/wait.ts
@@ -1,15 +1,16 @@
 export async function wait(minimum: number, maximum: number): Promise<string> {
-  return new Promise(resolve => {
-    if (isNaN(minimum) || isNaN(maximum)) {
-      throw new Error('minimum and maximum must be numbers')
-    }
+    return new Promise(resolve => {
+        if (isNaN(minimum) || isNaN(maximum)) {
+            throw new Error('minimum and maximum must be numbers')
+        }
 
-    if (minimum > maximum) {
-      throw new Error('minimum must be less than maximum')
-    }
+        if (minimum > maximum) {
+            throw new Error('minimum must be less than maximum')
+        }
 
-    const secs = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
+        const secs =
+            Math.floor(Math.random() * (maximum - minimum + 1)) + minimum
 
-    setTimeout(() => resolve(String(secs)), secs * 1000)
-  })
+        setTimeout(() => resolve(String(secs)), secs * 1000)
+    })
 }
