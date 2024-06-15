d30572ee331b95fcd2f7b7050b1acc9ddfc8f4fa
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-06-14 23:21:40 -0400

    build(deps): update to eslint v9 closes #121
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,30 +6,24 @@ import jest from 'eslint-plugin-jest'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import globals from 'globals'
 import tsParser from '@typescript-eslint/parser'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-})
 
 export default [
     {
         ignores: [
-            '**/dist/',
-            '**/lib/',
-            '**/node_modules/',
-            '**/jest.config.js',
-            '**/*.test.ts'
+            'dist/',
+            'lib/',
+            'node_modules/',
+            'jest.config.js',
+            '__tests__/'
         ]
     },
-    ...compat.extends('plugin:github/recommended'),
+    {
+        files: [
+            '**/*.ts',
+            '**/*.cts',
+            '**.*.mts'
+        ]
+    },
     {
         plugins: {
             jest,
