e20afb1dfe622d08a29cba0385308bfc8ec70447
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-06-26 22:29:32 -0400

    feat: updated the dependencies
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,24 +2,34 @@
 //
 // SPDX-License-Identifier: MIT
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import vitest from '@vitest/eslint-plugin';
+import jestPlugin from 'eslint-plugin-jest'
+import globals from 'globals'
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
     {
         ignores: [
-            'dist/',
-            'lib/',
-            '*.config.[tj]s',
-            'guide/'
+            '**/dist/**',
+            '**/lib/**'
         ]
     },
+    eslint.configs.recommended,
     {
-        files: ['*.ts'],
+        plugins: {
+            jest: jestPlugin,
+            '@typescript-eslint': tseslint.plugin
+        },
+
         languageOptions: {
+            globals: {
+                ...globals.node
+            },
+
+            parser: tseslint.parser,
+            ecmaVersion: 9,
+            sourceType: 'module',
+
             parserOptions: {
                 project: './tsconfig.eslint.json',
                 tsconfigRootDir: import.meta.dirname
@@ -38,5 +48,14 @@ export default tseslint.config(
             ...vitest.configs.recommended.rules, // you can also use vitest.configs.all.rules to enable all rules
             'vitest/max-nested-describe': ['error', { max: 3 }] // you can also modify rules' behavior using option like this
         }
+    },
+    {
+        files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
+        ...tseslint.configs.disableTypeChecked
+    },
+    {
+        // enable jest rules on test files
+        files: ['__test__/**'],
+        ...jestPlugin.configs['flat/recommended']
     }
-);
+)
