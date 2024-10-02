36ac7e268e7c9991cf5507732e0ed3ef0d147003
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-10-01 20:01:45 -0400

    feat: add an additional check to test for behavior
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,12 +3,8 @@
 // SPDX-License-Identifier: MIT
 
 import { wait } from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
-// import {expect, test} from '@jest/globals'
 
-test('throws invalid number', async () => {
+test('fails with invalid input', async () => {
     const minimum = parseInt('foo', 10)
     const maximum = parseInt('bar', 10)
     await expect(wait(minimum, maximum)).rejects.toThrow(
@@ -23,4 +19,28 @@ test('wait between 1 and 5 seconds', async () => {
     var delta = Math.abs(end.getTime() - start.getTime())
     expect(delta).toBeGreaterThanOrEqual(1000)
     expect(delta).toBeLessThanOrEqual(6000)
+})
+
+test('fails if minimum is higher than maximum', async () => {
+    const minimum = 10
+    const maximum = 5
+    await expect(wait(minimum, maximum)).rejects.toThrow(
+        'minimum must be less than maximum'
+    )
+})
+
+test('fails if minimum is higher than limit', async () => {
+    const minimum = 101
+    const maximum = 200
+    await expect(wait(minimum, maximum)).rejects.toThrow(
+        'minimum and maximum must be less than 100'
+    )
+})
+
+test('fails if maximum is higher than limit', async () => {
+    const minimum = 100
+    const maximum = 101
+    await expect(wait(minimum, maximum)).rejects.toThrow(
+        'minimum and maximum must be less than 100'
+    )
 })
--- src/wait.ts
+++ src/wait.ts
@@ -2,7 +2,11 @@
 //
 // SPDX-License-Identifier: MIT
 
-export async function wait(minimum: number, maximum: number): Promise<string> {
+const limit: number = 100
+
+type WaitPromise = Promise
+
+export async function wait(minimum: number, maximum: number): WaitPromise {
     return new Promise(resolve => {
         if (isNaN(minimum) || isNaN(maximum)) {
             throw new Error('minimum and maximum must be numbers')
@@ -10,6 +14,10 @@ export async function wait(minimum: number, maximum: number): Promise<string> {
 
         if (minimum > maximum) {
             throw new Error('minimum must be less than maximum')
+        }
+
+        if (minimum > limit || maximum > limit) {
+            throw new Error('minimum and maximum must be less than 100')
         }
 
         const secs =
