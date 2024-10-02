1ff9f611e044ceea9519248f18cf4ba663e49f06
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-10-01 20:01:45 -0400

    feat: add an additional check to test for behavior
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

---
+++ jest.setup.ts
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: 2022 - 2024 Ali Sajid Imami
+//
+// SPDX-License-Identifier: MIT
+
+import fc from 'fast-check'
+fc.configureGlobal({ numRuns: 100 }) // 1000 runs for all tests
