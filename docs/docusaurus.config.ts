2d590895658227f0ac27f74a8900e8d634876358
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-10-17 22:30:13 -0400

    docs: add docs

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,126 @@
+import { themes as prismThemes } from 'prism-react-renderer'
+import type { Config } from '@docusaurus/types'
+import type * as Preset from '@docusaurus/preset-classic'
+import { warn } from 'console'
+
+const config: Config = {
+    title: 'Random Wait Action',
+    tagline:
+        'A simple GitHub Action to inject wait for a random amount of time',
+    favicon: 'img/favicon.ico',
+
+    // Set the production url of your site here
+    url: 'https://aliimami.com',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/random-wait-action/',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'Ali Sajid Imami', // Usually your GitHub org/user name.
+    projectName: 'Random Wait Action', // Usually your repo name.
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en']
+    },
+
+    presets: [
+        [
+            'classic',
+            {
+                docs: {
+                    sidebarPath: './sidebars.ts'
+                },
+                theme: {
+                    customCss: './src/css/custom.css'
+                }
+            } satisfies Preset.Options
+
+        ]
+
+    ],
+
+    themeConfig: {
+        // Replace with your project's social card
+        image: 'img/docusaurus-social-card.jpg',
+        navbar: {
+            title: 'Random Wait Action',
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'tutorialSidebar',
+                    position: 'left',
+                    label: 'Tutorial'
+                },
+                {
+                    href: 'https://github.com/marketplace/actions/random-wait-action',
+                    position: 'left',
+                    label: 'GitHub Marketplace'
+                },
+                {
+                    href: 'https://github.com/AliSajid/random-wait-action',
+                    label: 'GitHub',
+                    position: 'right'
+                }
+            ]
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Tutorial',
+                            to: '/docs/intro'
+                        }
+                    ]
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/docusaurus'
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discordapp.com/invite/docusaurus'
+                        },
+                        {
+                            label: 'Twitter',
+                            href: 'https://twitter.com/docusaurus'
+                        }
+                    ]
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: '/blog'
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/facebook/docusaurus'
+                        }
+                    ]
+                }
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Ali Sajid Imami. Built with Docusaurus.`
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula
+        }
+    } satisfies Preset.ThemeConfig
+}
+
+export default config
