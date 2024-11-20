8d68f7500cda859b5658f64012016a144cc20633
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-11-20 13:20:50 -0500

    docs(reuse): added license information to the repository for REUSE compliance
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 - 2024 Ali Sajid Imami
+//
+// SPDX-License-Identifier: MIT
+
 import { themes as prismThemes } from 'prism-react-renderer'
 import type { Config } from '@docusaurus/types'
 import type * as Preset from '@docusaurus/preset-classic'
