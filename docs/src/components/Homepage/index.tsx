fde93ffbb2b8b372bef75138f7ef51dd51aef320
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-11-20 13:20:50 -0500

    docs(reuse): added license information to the repository for REUSE compliance
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- docs/src/components/Homepage/index.tsx
+++ docs/src/components/Homepage/index.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 - 2024 Ali Sajid Imami
+//
+// SPDX-License-Identifier: MIT
+
 import clsx from 'clsx'
 import Heading from '@theme/Heading'
 import styles from './styles.module.css'
