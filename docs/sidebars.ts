a1609a17ef33d8dd3cc69564dc4b83ce25c83cdc
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-11-20 13:20:50 -0500

    docs(reuse): added license information to the repository for REUSE compliance
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 - 2024 Ali Sajid Imami
+//
+// SPDX-License-Identifier: MIT
+
 import type { SidebarsConfig } from '@docusaurus/plugin-content-docs'
 
 /**
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 - 2024 Ali Sajid Imami
+//
+// SPDX-License-Identifier: MIT
+
 import clsx from 'clsx'
 import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
