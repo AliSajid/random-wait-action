c92f0f7f914fe563c581694676100bcd7da8e6f0
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-11-20 13:12:05 -0500

    docs(build): removed the unnecessary js files
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
     favicon: 'img/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://aliimami.com',
+    url: 'https://imamiland.com',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/random-wait-action/',
