abf0bee1f3c79f0223630248d6dbbe06ea3ad3e1
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-11-20 13:20:50 -0500

    docs(reuse): added license information to the repository for REUSE compliance
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // SPDX-FileCopyrightText: 2022 - 2024 Ali Sajid Imami
 //
 // SPDX-License-Identifier: MIT
@@ -19,37 +18,6 @@ export default function Home(): JSX.Element {
             description="Description will go into a meta tag in <head />"
         >
             <main></main>
-||||||| parent of 67a7c2b (docs: add docs)
-=======
-import clsx from 'clsx'
-import Link from '@docusaurus/Link'
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import Layout from '@theme/Layout'
-import Heading from '@theme/Heading'
-
-import styles from './index.module.css'
-
-export default function Home(): JSX.Element {
-    const { siteConfig } = useDocusaurusContext()
-    return (
-        <Layout
-            title={`Hello from ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />"
-        >
-<<<<<<< HEAD
-            <HomepageHeader />
-            <main>
-                <HomepageFeatures />
-            </main>
->>>>>>> 67a7c2b (docs: add docs)
-||||||| parent of fe230aa (docs: add documentation)
-            <HomepageHeader />
-            <main>
-                <HomepageFeatures />
-            </main>
-=======
-            <main></main>
->>>>>>> fe230aa (docs: add documentation)
         </Layout>
     )
 }
