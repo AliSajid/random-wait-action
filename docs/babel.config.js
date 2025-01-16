ba8bdff81e67a83e82aa4172f934e49a62afe4ed
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-01-16 10:44:09 -0500

    feat: update the build with tests

--- docs/babel.config.js
+++ docs/babel.config.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-// SPDX-FileCopyrightText: 2022 - 2024 Ali Sajid Imami
-//
-// SPDX-License-Identifier: CC0-1.0
-
-||||||| parent of 67a7c2b (docs: add docs)
-=======
->>>>>>> 67a7c2b (docs: add docs)
 module.exports = {
     presets: [require.resolve('@docusaurus/core/lib/babel/preset')]
 }
