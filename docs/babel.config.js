8b9fe7c3fa19fa7e41401cc7b4ff85e9cffaea96
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-01-16 10:44:09 -0500

    feat: update the build with tests

--- docs/babel.config.js
+++ docs/babel.config.js
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 // SPDX-FileCopyrightText: 2022 - 2024 Ali Sajid Imami
 //
 // SPDX-License-Identifier: CC0-1.0
 
-||||||| parent of 67a7c2b (docs: add docs)
-=======
->>>>>>> 67a7c2b (docs: add docs)
-||||||| parent of 8570590 (docs(reuse): added license information to the repository for REUSE compliance)
-=======
-// SPDX-FileCopyrightText: 2022 - 2024 Ali Sajid Imami
-//
-// SPDX-License-Identifier: CC0-1.0
-
->>>>>>> 8570590 (docs(reuse): added license information to the repository for REUSE compliance)
 module.exports = {
     presets: [require.resolve('@docusaurus/core/lib/babel/preset')]
 }
