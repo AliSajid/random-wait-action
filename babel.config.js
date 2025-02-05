4ab95e8943b0b4a097b33519e67fc21ac25ad75d
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-02-05 13:36:35 -0500

    build: remove docusaurus docs

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2022 - 2024 Ali Sajid Imami
-//
-// SPDX-License-Identifier: CC0-1.0
-
-module.exports = {
-    presets: [require.resolve('@docusaurus/core/lib/babel/preset')]
-}
