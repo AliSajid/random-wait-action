19bde05c48b9125b98635783be55448e5d3c6edd
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-02-26 19:49:32 -0500

    feat: add MAXIMUM_ALLOWED and MINIMUM_ALLOWED limits
    I have now introduced validation checks that `minimum` and `maximum`
    values are greater than 0. This also marks the change of the
    maximum allowable time limit to two minutes (120 seconds)
    
    BREAKING:

--- src/wait.ts
+++ src/wait.ts
@@ -2,8 +2,11 @@
 //
 // SPDX-License-Identifier: MIT
 
+// Set the minimum time limit to 0 seconds
+export const MINIMUM_ALLOWED: number = 0
+
 // Set the maximum time limit to 2 minutes (120 seconds)
-const limit: number = 120
+export const MAXIMUM_ALLOWED: number = 120
 
 type WaitPromise = Promise<string>
 
@@ -23,12 +26,30 @@ export async function wait(minimum: number, maximum: number): WaitPromise {
             throw new Error('minimum and maximum must be numbers')
         }
 
+        if (!Number.isInteger(minimum) || !Number.isInteger(maximum)) {
+            throw new Error(
+                'minimum and maximum values must be positive integers'
+            )
+        }
+
+        if (minimum > MAXIMUM_ALLOWED || maximum > MAXIMUM_ALLOWED) {
+            throw new Error(
+                `minimum and maximum must be less than ${MAXIMUM_ALLOWED}`
+            )
+        }
+
+        if (minimum < MINIMUM_ALLOWED || maximum < MINIMUM_ALLOWED) {
+            throw new Error(
+                `minimum and maximum values must be greater than ${MINIMUM_ALLOWED}`
+            )
+        }
+
         if (minimum > maximum) {
             throw new Error('minimum must be less than maximum')
         }
 
-        if (minimum > limit || maximum > limit) {
-            throw new Error('minimum and maximum must be less than 120')
+        if (maximum < minimum) {
+            throw new Error('maximum must be greater than minimum')
         }
 
         const secs =
