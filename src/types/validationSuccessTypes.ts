0ba550d4e6cdefe00bd4b87b938edf0ed8139d89
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-02-28 12:32:28 -0500

    feat: add types for errors and success

---
+++ src/types/validationSuccessTypes.ts
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: 2022 - 2024 Ali Sajid Imami
+//
+// SPDX-License-Identifier: MIT
+
+export interface InputValidationSuccess {
+    type: 'InputValidationSuccess'
+}
+
+export class InputValidationSuccess implements InputValidationSuccess {
+    type: 'InputValidationSuccess' = 'InputValidationSuccess'
+}
