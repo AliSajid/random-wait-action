1ad02c8312735dff5e3312db4ea7496aa839af20
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-04-11 18:19:02 -0400

    test: isolate vitest and eslint's typescript config

--- __tests__/wait.test.ts
+++ __tests__/wait.test.ts
@@ -7,38 +7,39 @@ import { wait } from '../src/wait'
 
 const timingBuffer = 1000 // milliseconds to account for timer inaccuracy and async delays
 
-describe('wait', () => {
+describe('wait (real setTimeout)', () => {
     it('resolves between 1 and 5 seconds', async () => {
+        expect.hasAssertions()
+
         const start = Date.now()
         await wait(1, 5)
         const end = Date.now()
 
         const delta = end - start
+
         expect(delta).toBeGreaterThanOrEqual(1000)
         expect(delta).toBeLessThanOrEqual(5000 + timingBuffer)
     })
 
     it('resolves between 5 and 10 seconds', async () => {
+        expect.hasAssertions()
+
         const start = Date.now()
         await wait(5, 10)
         const end = Date.now()
 
         const delta = end - start
+
         expect(delta).toBeGreaterThanOrEqual(5000)
         expect(delta).toBeLessThanOrEqual(10000 + timingBuffer)
     })
 })
 
 describe('wait (mocked setTimeout)', () => {
-    beforeEach(() => {
-        vi.useFakeTimers()
-    })
-
-    afterEach(() => {
-        vi.useRealTimers()
-    })
-
     it('waits and resolves with a number string in range', async () => {
+        expect.hasAssertions()
+
+        vi.useFakeTimers()
         const promise = wait(2, 4) // should pick 2, 3, or 4
         // advance time to max possible delay to ensure resolution
         vi.advanceTimersByTime(4000)
@@ -55,5 +56,6 @@ describe('wait (mocked setTimeout)', () => {
                 throw new Error('Expected Ok but got Err')
             }
         })
+        vi.useRealTimers()
     })
 })
