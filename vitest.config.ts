204ece24bfe2233a600b1d9e59ccb650de90f071
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-04-11 21:11:20 -0400

    tests: allow coverage json to be generated

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,30 +2,16 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
         globals: true, // So you don't need to import `describe`, `it`, etc.
         environment: 'node',
         coverage: {
-            enabled: true,
             provider: 'v8', // Or 'c8'
-            reporter: ['text', 'html', 'json', 'json-summary', 'lcov'],
-            exclude: [
-                '**/*.test.ts',
-                '**/__tests__/**',
-                'dist/',
-                'eslint.config.mjs',
-                'guide/',
-                'lib',
-                'node_modules',
-                'node_modules/**',
-                'rollup.config.ts',
-                'src/main.ts',
-                'vitest.config.ts'
-            ]
+            reporter: ['text', 'html', 'json']
         },
         testTimeout: 30000
     }
-});
+})
