478b2233b39d0e79044ee6ab590adcb03bb1b35f
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-04-23 10:25:20 -0400

    build: add a rollup config

---
+++ rollup.config.ts
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: 2022 - 2025 Ali Sajid Imami
+//
+// SPDX-License-Identifier: MIT
+
+// See: https://rollupjs.org/introduction/
+
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+
+const config = {
+    input: 'src/index.ts',
+    output: {
+        esModule: true,
+        file: 'dist/index.js',
+        format: 'es',
+        sourcemap: true
+    },
+    plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
+}
+
+export default config
