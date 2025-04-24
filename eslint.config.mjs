63136425d9f9b819fa9fc0095c912b31d061c114
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2025-04-24 18:05:40 -0400

    build: update the eslint and tsconfig configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,105 +2,54 @@
 //
 // SPDX-License-Identifier: MIT
 
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import stylistic from '@stylistic/eslint-plugin'
-import vitest from '@vitest/eslint-plugin'
-import globals from 'globals'
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+import vitest from '@vitest/eslint-plugin';
 
 export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
     {
         ignores: [
-            '**/dist/**',
-            '**/lib/**',
-            'docs/**'
+            'dist/',
+            'lib/',
+            '*.config.[tj]s'
         ]
     },
-    eslint.configs.recommended,
+    {
+        files: ['*.ts'],
+        languageOptions: {
+            parserOptions: {
+                project: './tsconfig.eslint.json',
+                tsconfigRootDir: import.meta.dirname
+            }
+        }
+    },
+
+    tseslint.configs.stylistic,
+
     {
         plugins: {
-            '@typescript-eslint': tseslint.plugin,
             '@stylistic': stylistic
         },
-
-        languageOptions: {
-            globals: {
-                ...globals.node
-            },
-
-            parser: tseslint.parser,
-            ecmaVersion: 9,
-            sourceType: 'module',
-
-            parserOptions: {
-                project: './tsconfig.eslint.json'
-            }
-        },
-
         rules: {
-            '@typescript-eslint/array-type': 'error',
-            '@typescript-eslint/await-thenable': 'error',
-            '@typescript-eslint/ban-ts-comment': 'error',
-            '@typescript-eslint/consistent-type-assertions': 'error',
-
-            '@typescript-eslint/explicit-function-return-type': [
-                'error',
-                {
-                    allowExpressions: true
-                }
-            ],
-
-            '@typescript-eslint/explicit-member-accessibility': [
-                'error',
-                {
-                    accessibility: 'no-public'
-                }
-            ],
-
-            '@stylistic/func-call-spacing': ['error', 'never'],
-            '@typescript-eslint/no-array-constructor': 'error',
-            '@typescript-eslint/no-empty-interface': 'error',
-            '@typescript-eslint/no-explicit-any': 'error',
-            '@typescript-eslint/no-extraneous-class': 'error',
-            '@typescript-eslint/no-for-in-array': 'error',
-            '@typescript-eslint/no-inferrable-types': 'error',
-            '@typescript-eslint/no-misused-new': 'error',
-            '@typescript-eslint/no-namespace': 'error',
-            '@typescript-eslint/no-non-null-assertion': 'warn',
-            '@typescript-eslint/no-require-imports': 'error',
-            '@typescript-eslint/no-unnecessary-qualifier': 'error',
-            '@typescript-eslint/no-unnecessary-type-assertion': 'error',
-            '@typescript-eslint/no-unused-vars': 'error',
-            '@typescript-eslint/no-useless-constructor': 'error',
-            '@typescript-eslint/no-var-requires': 'error',
-            '@typescript-eslint/prefer-for-of': 'warn',
-            '@typescript-eslint/prefer-function-type': 'warn',
-            '@typescript-eslint/prefer-includes': 'error',
-            '@typescript-eslint/prefer-string-starts-ends-with': 'error',
-            '@typescript-eslint/promise-function-async': 'error',
-            '@typescript-eslint/require-array-sort-compare': 'error',
-            '@typescript-eslint/restrict-plus-operands': 'error',
-            '@stylistic/semi': ['error', 'never'],
-            '@stylistic/type-annotation-spacing': 'error',
-            '@typescript-eslint/unbound-method': 'error',
-            camelcase: 'off',
-            'eslint-comments/no-use': 'off',
-            'i18n-text/no-en': 'off',
-            'import/no-namespace': 'off',
-            'no-unused-vars': 'off',
-            semi: 'off'
+            // stylistic recommendations (edit as needed)
+            '@stylistic/indent': ['error', 4],
+            '@stylistic/semi': ['error', 'always'],
+            '@stylistic/quotes': ['error', 'single'],
+            '@stylistic/comma-dangle': ['error', 'always-multiline']
         }
     },
+
     {
-        files: ['**/*.js', '**/*.cjs', '**/*.mjs'],
-        ...tseslint.configs.disableTypeChecked
-    },
-    {
-        files: ['__tests__/*.test.ts'],
-        plugins: { vitest },
+        files: ['__tests__/**', 'src/**/*.spec.ts'], // or any other pattern
+        plugins: {
+            vitest
+        },
         rules: {
-            ...vitest.configs.all.rules,
-            'vitest/max-nested-describe': ['error', { max: 3 }]
+            ...vitest.configs.recommended.rules, // you can also use vitest.configs.all.rules to enable all rules
+            'vitest/max-nested-describe': ['error', { max: 3 }] // you can also modify rules' behavior using option like this
         }
     }
-)
+);
