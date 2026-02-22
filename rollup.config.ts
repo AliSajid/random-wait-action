1099a64fe2ea10de61b49ce3b732b4bf695663f4
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2026-02-21 22:48:10 -0500

    feat: ensure reproducible builds for the bundle

--- rollup.config.ts
+++ rollup.config.ts
@@ -6,15 +6,39 @@
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
 
 const config = {
     input: 'src/main.ts',
     output: {
         file: 'dist/index.cjs',
         format: 'cjs',
-        sourcemap: true
+        sourcemap: true,
+        indent: false,
+        compact: true,
+        validate: true,
+        plugins: [
+            terser({
+                compress: {
+                    passes: 2
+                },
+                mangle: {
+                    keep_fnames: false,
+                    toplevel: true
+                },
+                format: {
+                    comments: false
+                },
+                ecma: 2020,
+                module: false
+            })
+        ]
     },
-    plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()]
+    plugins: [
+        typescript(),
+        nodeResolve({ preferBuiltins: true }),
+        commonjs()
+    ]
 }
 
 export default config
